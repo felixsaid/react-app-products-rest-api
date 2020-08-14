ed59347a0afb59e671782a62b37fc747a20ff0b7
felixsaid <kiambafelix@yahoo.com>
2020-08-14 20:16:26 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Product from "./components/Product";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    const response = await fetch("http://localhost:49341/v1/api/products/", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+
+    const data = await response.json();
+    setProducts(data.data);
+    console.log(data.data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            Produts App
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarResponsive"
+            aria-controls="navbarResponsive"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  Home
+                  <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Products
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12 text-center">
+            <h1 className="mt-5">A React Products/Orders - Consuming API</h1>
+            <p className="lead">
+              This is a simple example with ReactJS - APIs have been developed
+              with .NET Core
+            </p>
+            <br />
+            <hr /> <br />
+            <Product
+              product_name="Bananas"
+              product_price="200"
+              category="Fruits"
+              quantity_in_stock="10"
+              created_by="Lucy Nzili"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
